85c344e39f14c76353e3846703fcc0685c274b14
Yaroslav Sidlovsky <zawertun@gmail.com>
2017-03-20 20:30:34 +0400

    Release -> Debug

--- ILMerge.cmd
+++ ILMerge.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /target:winexe /v4 /out:ImapTray.exe bin\Release\ImapTray.exe bin\Release\S22.Imap.dll
+"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /target:winexe /v4 /out:ImapTray.exe bin\Debug\ImapTray.exe bin\Debug\S22.Imap.dll
